d93f9bfc89ce6ea1288d75671398c8f5353637b7
Volkan GÜRGÖR <volkangurgor@gmail.com>
2023-06-13 10:53:42 +0300

    direct run result retun

--- optimizationWizard.py
+++ optimizationWizard.py
@@ -38,6 +38,7 @@ class OptimizationWizard:
             school = SchoolScheduling(params['days'], params['periods'], params['courses'], params['teachers'], params['classrooms'])
             school.optimization()
             school.print_results()
+            self.getMission()
             
 
     def getDataFromJson (self, jsonFile=''):
